1999941220159ec04027ddb9e8ae5296e45d7c66
jgouveia-lab <265724459+joaorobalogouveia@users.noreply.github.com>
2026-05-29 12:24:50 +0200

    Iteration 2 done

--- src/question.js
+++ src/question.js
@@ -1,7 +1,15 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+  shuffleChoices() {
+    for (let i = 0; i < this.choices.length; i++) {
+      const j = Math.floor(Math.random() * this.choices.length);
+    [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]
+    }
+  }
 }
+
